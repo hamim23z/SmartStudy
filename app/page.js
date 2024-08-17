b3347648aba33088b1c45a39c0e56eb37a6d2cef
Hamim <hamimc232@gmail.com>
2024-08-16 21:29:26 -0400

    Basic Layout of Landing Page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,58 @@
 import Image from "next/image";
-
+import getStripe from "@/utils/get-stripe";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  Box,
+  Container,
+  Typography,
+  Stack,
+  AppBar,
+  Toolbar,
+  Button,
+} from "@mui/material";
+import Head from "next/head";
 
 export default function Home() {
-  return {
-    
-  }
+  return (
+    <Container maxWidth="100vw">
+      <Head>
+        <title>Smart Study</title>
+        <meta
+          name="description"
+          content="Create flashcards from your text"
+        ></meta>
+      </Head>
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Smart Study
+          </Typography>
+          <SignedOut>
+            <Button style={{ color: "black" }}>Login</Button>
+            <Button style={{ color: "black" }}>Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton></UserButton>
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        sx={{
+          textAlign: "center",
+          my: 4
+        }}
+      >
+        <Typography variant="h2">Welcome to Smart Study</Typography>
+        <Typography variant="subtitle1">
+          {' '}
+          The premiere flashcards and study materials for Computer Science Students
+        </Typography>
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          Get Started
+        </Button>
+      </Box>
+    </Container>
+  );
 }
