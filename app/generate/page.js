ec6a87464942d6925f0180249f80ed08616e8007
Hamim <hamimc232@gmail.com>
2024-08-17 18:44:31 -0400

    Basic outline of Generate Flashcards Page

---
+++ app/generate/page.js
@@ -0,0 +1,86 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { getDoc, writeBatch } from "firebase/firestore";
+import { Container } from "@mui/material";
+
+export default function Generate() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    fetch("api/generate", {
+      method: "POST",
+      body: text,
+    })
+      .then((res) => res.json())
+      .then(data > setFlashcards(data));
+  };
+
+  const handleCardClick = (id) => {
+    setFlipped((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const saveFlashcards = async () => {
+    if (!name) {
+        alert(`Please enter a name for the flashcards!`)
+        return
+    }
+
+
+    const batch = writeBatch(db)
+    const userDocRef = doc(collection(db, "users"), user.id)
+    const docSnap = await getDoc(userDocRef)
+
+    if (docSnap.exists()) {
+        const collections = docSnap.data().flashcards || []
+        if (collections.find((f) => f.name === name)) {
+            alert("Flashcard collection with the same name already exists")
+            return
+        }
+        else {
+            collections.push({name})
+            batch.set(userDocRef, { flashcardSets: [{ name: setName }] })
+        }
+    }
+    else {
+        batch.set(userDocRef, {flashcards: [{name}]})
+    }
+
+    const colRef = collection(userDocRef, name)
+    flashcards.forEach((flashcard) => {
+        const cardDocRef = doc(colRef)
+        batch.set(cardDocRef, flashcard)
+    })
+
+    await batch.commit()
+    handleClose()
+    router.push('/flashcards')
+  }
+
+
+  return (
+    <Container>
+
+    </Container>
+  )
+
+
+}
